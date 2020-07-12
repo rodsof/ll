3a38ea90d12b547f5d6d8d30b375b23a1ac225e9
sofi <sofia9803@hotmail.es>
2020-07-12 18:20:38 -0300

    CONFIGURE .ENV

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-// arrancar la app
+// Start app
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server working at port ${port}`);
 });
